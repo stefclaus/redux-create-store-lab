bb745ebaac79ecff3d3154e4186892f67027c38b
github username <github email address>
2020-09-24 10:33:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -17,6 +17,29 @@ function render() {
     throw new Error("the store's state has not been defined yet")
   }
 };
+//
+   function createStore(reducer) {
+     let state;
+  //
+     function dispatch(action) {
+       state = reducer(state, action);
+       render();
+     }
+
+    function getState() {
+      return state;
+    };
+  //
+    return {
+      dispatch,
+      getState
+    };
+
+   }
+
+
+let store = createStore(candyReducer) // createStore takes the reducer as an argument
+store.dispatch({ type: '@@INIT' });
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
